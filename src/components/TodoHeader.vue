396fc12677990092e9089cb853c27565b85c8ac1
parkhyeongkyu <phg0644@gmail.com>
2020-07-01 17:04:26 +0900

    vue 개발 준비

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            title: 'TODO it!'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
